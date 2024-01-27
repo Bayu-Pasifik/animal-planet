c0ec7e61ed91b2f1ee4c26eb242164860462bc37
milky_way <shin.kagami13@gmail.com>
2024-01-27 12:35:27 +0700

    (+) Add Zoom image in Tab Album

--- lib/app/data/widgets/tabalbum.dart
+++ lib/app/data/widgets/tabalbum.dart
@@ -1,29 +1,58 @@
 import 'package:animal_planet/app/data/models/animals.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 Widget buildAlbumTab(AnimalsModel animal) {
-    // Kustomisasi tampilan untuk tab Album
-    return GridView.builder(
-      padding: const EdgeInsets.all(10),
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 1 / 1.5,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-      ),
-      itemCount: animal.imageLink?.length ?? 0,
-      itemBuilder: (context, index) {
-        // Tambahkan widget untuk menampilkan album hewan
-        var gambar = animal.imageLink![index]
-            .replaceAll("/view?usp=sharing", "")
-            .replaceAll("/view?usp=drive_link", "")
-            .replaceAll("/d/", "")
-            .replaceAll(
-                "https://drive.google.com/", "https://drive.google.com/uc?id=")
-            .replaceAll("file", "");
-        return CachedNetworkImage(
+  return GridView.builder(
+    padding: const EdgeInsets.all(10),
+    gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+      maxCrossAxisExtent: 200,
+      childAspectRatio: 1 / 1.5,
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 20,
+    ),
+    itemCount: animal.imageLink?.length ?? 0,
+    itemBuilder: (context, index) {
+      var gambar = animal.imageLink![index]
+          .replaceAll("/view?usp=sharing", "")
+          .replaceAll("/view?usp=drive_link", "")
+          .replaceAll("/d/", "")
+          .replaceAll(
+              "https://drive.google.com/", "https://drive.google.com/uc?id=")
+          .replaceAll("file", "");
+      return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              allowSnapshotting: true,
+              builder: (context) => ExtendedImageSlidePage(
+                slideAxis: SlideAxis.both,
+                slideType: SlideType.wholePage,
+                
+                onSlidingPage: (state) {},
+                child: CachedNetworkImage(
+                  imageUrl: gambar,
+                  imageBuilder: (context, imageProvider) => Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6.r),
+                      image: DecorationImage(
+                        image: imageProvider,
+                      ),
+                    ),
+                  ),
+                  placeholder: (context, url) =>
+                      const Center(child: CircularProgressIndicator()),
+                  errorWidget: (context, url, error) =>
+                      Image.asset("assets/images/Image_not_available.png"),
+                ),
+              ),
+            ),
+          );
+        },
+        child: CachedNetworkImage(
           imageUrl: gambar,
           imageBuilder: (context, imageProvider) => Container(
             decoration: BoxDecoration(
@@ -38,7 +67,8 @@ Widget buildAlbumTab(AnimalsModel animal) {
               const Center(child: CircularProgressIndicator()),
           errorWidget: (context, url, error) =>
               Image.asset("assets/images/Image_not_available.png"),
-        );
-      },
-    );
-  }
+        ),
+      );
+    },
+  );
+}
